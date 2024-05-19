12075a8b455954c81bce16e2208e5f5468d18e04
drzezga <19654689+drzezga@users.noreply.github.com>
2024-05-19 09:57:02 +0200

    FIxed a websocket mistake

--- api/frontend_api.py
+++ api/frontend_api.py
@@ -4,16 +4,25 @@ import websockets
 from models.messages.StatusMessage import StatusMessage
 
 class FrontendApi: 
-    def __init__(self):
-        self.websocket = websockets.connect('ws://128.0.0.1:8765')
-        
+    async def __init__(self):
+        self.websocket = await websockets.serve(self.handle_connection, 'localhost', 8765)
+        # self.websocket = websockets.connect('ws://128.0.0.1:8765')
+
+    async def handle_connection(self, websocket, path):
+        self.websocket = websocket
+        while True:
+            message = await websocket.recv()
+
+
     #{type: detections, detections: [drone_id: 5, object_class: 43, timestamp: 2343222, lat: 51.32332, lon: 22.432432]}
     async def send_map(self, map_message):
         detections = map_message.detections
         map_message = {"type": "detections", "detections": detections}
-        await self.websocket.send(json.dumps(map_message))
+        for socket in self.websocket.sockets:
+            await socket.send(json.dumps(map_message))
 
     #{type: status, drones_online: 54}
     async def send_status(self, status_message: StatusMessage):
         status_message = {"type": "status", "drones_online": 54}
-        await self.websocket.send(json.dumps(status_message))
+        for socket in self.websocket.sockets:
+            await socket.send(json.dumps(status_message))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from api.frontend_api import FrontendApi
 from src.overmind import Overmind
 import asyncio
 
 if __name__ == "__main__":
     overmind = Overmind()
     asyncio.run(overmind.think())
+    frontend_api = FrontendApi()
