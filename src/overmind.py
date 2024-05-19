805d975a323e8347b2cb1f3e00e9a021dc739495
drzezga <19654689+drzezga@users.noreply.github.com>
2024-05-19 09:39:45 +0200

    Implemented simple overmind behavior

--- src/overmind.py
+++ src/overmind.py
@@ -1,8 +1,10 @@
+import asyncio
 import json
 from redis import Redis
 
 from src.position import Position
-
+import random
+import math
 
 class Overmind:
     def __init__(self):
@@ -11,10 +13,12 @@ class Overmind:
         self.pubsub.psubscribe('position:*')
         self.pubsub.psubscribe('detection:*')
         self.drone_positions = {}
-        self.detected_objects = {}
+        self.detected_objects = []
 
-    def think(self):
+    async def think(self):
         for message in self.pubsub.listen():
+            # Sleep to allow other tasks to run (if necessary)
+            await asyncio.sleep(0)
             print(message)
             try:
                 if message['type'] == 'pmessage':
@@ -24,7 +28,14 @@ class Overmind:
                     if str(message) == "b'position":
                         self.drone_positions[drone_id] = Position(data["lat"], data["lon"], data["alt"], data["attitude"])
                     if message == "b'detection":
-                        print(data)
+                        self.detected_objects.append(data)
+                        random_angle = random.randint(0, 360) / 360 * 2 * math.pi
+
+                        # 15 meters in latitude and longitude
+                        offset_vector = (math.cos(random_angle) * 15 / 111_139, math.sin(random_angle) * 15 / 111_139)
+
+                        # Follow the detected object
+                        self.fly_to(drone_id, data["lat"] + offset_vector[0], data["lon"] + offset_vector[1])
             except Exception as e:
                 print(f"Error when decoding redis message\n{e}")
     def fly_to(self, drone_id, lat, lon):
